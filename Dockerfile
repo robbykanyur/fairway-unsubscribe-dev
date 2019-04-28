34a4ac2f3eff9b5b8bffb69c1b6d6c8d1a9b0030
Robert Kanyur <robert@kanyur.com>
2019-04-27 18:58:31 -0700

    Added basic sentry information

--- Dockerfile
+++ Dockerfile
@@ -3,14 +3,14 @@ RUN adduser -D unsubscribe
 
 WORKDIR /home/unsubscribe
 
-RUN apk add --no-cache --virtual .build-deps gcc musl-dev libffi-dev openssl-dev
+RUN apk add --no-cache --virtual .build-deps gcc musl-dev libffi-dev openssl-dev make
 
 COPY requirements.txt requirements.txt
 RUN python -m venv venv
 RUN venv/bin/pip install -r requirements.txt
 RUN venv/bin/pip install gunicorn pymysql
 
-RUN apk del .build-deps gcc musl-dev libffi-dev openssl-dev
+RUN apk del .build-deps gcc musl-dev libffi-dev openssl-dev make
 
 COPY boot.sh boot.dev.sh ./
 RUN chmod +x boot*
--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,4 +1,5 @@
-from os import path
+import os
+import base64
 from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -6,12 +7,22 @@ from config import Config
 from .momentjs import momentjs
 from flask_login import LoginManager
 from flask_webpack import Webpack
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
 
 db = SQLAlchemy()
 migrate = Migrate()
 login = LoginManager()
 webpack = Webpack()
 login.login_view = 'main.login'
+
+FLASK_ENV = os.getenv('FLASK_ENV')
+SENTRY_DSN = base64.b64decode(os.getenv('SENTRY_DSN')).decode('utf-8')
+if (FLASK_ENV == 'production'):
+    sentry_sdk.init(
+       dsn=SENTRY_DSN,
+       integrations=[FlaskIntegration()]
+    )
 
 def create_app(config_class=Config):
     app = Flask(__name__)
